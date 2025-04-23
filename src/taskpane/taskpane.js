a0bc69438d413457aee9ede7c742e1029a4b7c01
Y6kEld <esll.s@qq.com>
2025-04-24 07:47:01 +0800

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -20,7 +20,7 @@ export async function run() {
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("2025.04.23 taskpane.js,可修改显示文本", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("2025.04.24 taskpane.js,可修改显示文本", Word.InsertLocation.end);
 
     // change the paragraph color to green.
     paragraph.font.color = "blue";
