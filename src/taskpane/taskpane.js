3676523722f17358c8afc395a6210666a8665d18
Y6kEld <esll.s@qq.com>
2026-04-12 00:13:29 +0800

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -20,7 +20,7 @@ export async function run() {
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("2026.03.26   打开taskpane.js修改显示文本", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("2026.04.12   打开taskpane.js修改显示文本", Word.InsertLocation.end);
 
     // change the paragraph color to green.
     paragraph.font.color = "blue";
